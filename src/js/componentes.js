f4c1790a377fa7f4a29bdb17132a2f361e07a6bf
akerman <akerman@akerman>
2022-04-03 10:13:15 -0300

    first commit

---
+++ src/js/componentes.js
@@ -0,0 +1,15 @@
+import '../css/componentes.css';
+// import webpackLogo from '../assets/img/webpack-logo.png';
+
+export const saludar = (nombre) => {
+    console.log('creando h1');
+    const h1 = document.createElement('h1');
+    h1.innerText = `hola ${nombre}!`;
+
+    document.body.append(h1);
+
+    // console.log(webpackLogo)
+    // const img = document.createElement('img');
+    // img.src = webpackLogo;
+    // document.body.append(img);
+}
